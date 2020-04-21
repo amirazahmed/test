6d950fa6b60920ce63c274703d23e7748349a10d
amirazahmed <amira.ahmed199620@gmail.com>
2020-04-22 00:59:45 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from "@angular/core";
+
+import { Platform, ActionSheetController } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
+})
+export class AppComponent {
+  test = [{ name: "amiarraaaa" }, { name: "sara" }];
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    public actionSheetController: ActionSheetController
+  ) {
+    this.initializeApp();
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: "Sort by",
+      buttons: [
+        {
+          text: "delete sara",
+          handler: () => {
+            console.log("test1");
+
+            this.sort();
+          }
+        },
+        {
+          text: "Cancel",
+          icon: "close",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel clicked");
+          }
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+  sort() {
+    this.test = [{ name: "amiarraaaa", body: "aya", bodyn: "shimaaa" }];
+  }
+}
